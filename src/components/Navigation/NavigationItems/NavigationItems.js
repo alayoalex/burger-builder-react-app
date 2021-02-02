d0463b1f2c3abec4fdf0b1cbe127234d0d0f381b
Alexei Alayo <alexei.alayo@gmail.com>
2021-02-02 12:58:48 -0500

    Working in navigation and toolbar

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './NavigationItems.css'
+import NavigationItem from './NavigationItem/NavigationItem'
+
+const navigationItems = (props) => (
+    <ul className={"NavigationItems"}>
+        <NavigationItem link='/' active>BurguerBuilder</NavigationItem>
+        <NavigationItem link='/'>Checkout</NavigationItem>
+    </ul>
+);
+
+export default navigationItems;
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import './Toolbar.css';
+
+const toolbar = (props) => (
+    <header className={'Toolbar'}>
+        <div>MENU</div>
+        <div className={'LogoToolbar'}>
+            <Logo />
+        </div>
+        <nav className={'DesktopOnly'}>
+            <NavigationItems />
+        </nav>
+    </header>
+);
+
+export default toolbar;
